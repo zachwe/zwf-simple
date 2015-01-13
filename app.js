fe7037819004758932bf906335e3299575511b5d
zachwe <zbwener@gmail.com>
2015-01-12 19:15:51 -0500

    removed unnecessary stuff.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var debug = require('debug')('zwf-simple');
 var bodyParser = require('body-parser');
 
@@ -16,7 +15,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
